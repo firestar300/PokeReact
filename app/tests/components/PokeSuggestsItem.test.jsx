c48555e673407f345d2c2882295cdb02b4529f32
Milan Ricoul <milan.ricoul@gmail.com>
2017-04-30 21:49:19 +0200

    Add PokeSuggestItem test

---
+++ app/tests/components/PokeSuggestsItem.test.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+
+var PokeSuggestsItem = require('PokeSuggestsItem');
+
+describe('PokeSuggestsItem', () => {
+  it('should exist', () => {
+    expect(PokeSuggestsItem).toExist();
+  });
+});
